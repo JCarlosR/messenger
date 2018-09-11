6d7202f60975fd42496b470dc906c0e0ced3f002
Juan Ramos <juancagb.17@gmail.com>
2018-09-10 21:16:00 -0500

    Add v-cloak to show a loading indicator

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,24 +1,37 @@
-<!DOCTYPE html class="h-100">
-<html lang="{{ app()->getLocale() }}">
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}" class="h-100">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Styles -->
-
+    {{-- Styles --}}
+    <style>
+        [v-cloak] > * {
+            display: none;
+        }
+        [v-cloak]::before {
+            content: " ";
+            display: block;
+            position: absolute;
+            width: 80px;
+            height: 80px;
+            background-image: url(/images/loader.svg);
+            background-size: cover;
+            left: 50%;
+            top: 50%;
+        }
+    </style>
 </head>
 <body class="h-100">
     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
         {{ csrf_field() }}
     </form>
 
-    <div id="app" class="h-100">
+    <div id="app" class="h-100" v-cloak>
         <b-navbar toggleable type="dark" variant="primary">
             <b-navbar-toggle target="nav_text_collapse"></b-navbar-toggle>
 
